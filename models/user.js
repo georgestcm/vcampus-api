d9b61fb6a058744c26c3a755436bbce96c78f730
georges tchianga <georgestcm@gmail.com>
2020-04-17 00:01:29 -0400

    Add new routes for user auth and course

--- models/user.js
+++ models/user.js
@@ -5,16 +5,84 @@ const schema = mongoose.Schema;
 //schema for geolocation and others
 
 const userSchema = new schema({
-  username: String,
-  password: String,
-  first_name: String,
-  last_name:String,
-  email: String,
-  phone_number: Number,
-  profile_photo: String,
+  username: {
+    type: String,
+    default: null
+  },
+  password: {
+    type: String,
+    default: null
+  },
+  first_name: {
+    type: String,
+    default: null
+  },
+  last_name:{
+    type: String,
+    default: null
+  },
+  email: {
+    type: String,
+    default: null
+  },
+  phone_number: {
+    type: Number,
+    default: null
+  },
+  profile_photo: {
+    type: String,
+    default: null
+  },
   roles:[{
     type: Number
-}]
+}],
+school:{
+  school_name:{
+    type:String,
+    default:null
+  },
+  principal_first_name:{
+    type:String,
+    default:null
+  },
+  principal_last_name:{
+    type:String,
+    default:null
+  },
+  access: {
+    type:Boolean,
+    default: false
+  },
+  registration_valid:{
+    type:Boolean,
+    default:false
+  },
+  description: {
+    type:String,
+    default:null
+  },
+  curriculums:[
+    {
+      type:String,
+      default:null
+    }
+  ],
+  teacher:[{
+    _id:{
+      type:String,
+      default:null
+    },
+    first_name:{
+      type:String,
+      default:null
+    },
+    last_name:{
+      type:String,
+      default:null
+    }
+  }]
+}
+
 })
 
 module.exports = mongoose.model('user',userSchema,'users')
--- routes/api.js
+++ routes/api.js
@@ -25,15 +25,46 @@ router.get('/',(req,res)=>{
 
 })
 
+router.get('/get_curriculum_list',(req,res)=>{
+  console.log(req.query)
+  User.findOne({_id:req.query._id},(err,data)=>{
+    if(err){
+      res.status(401).send(err)
+    } else {
+      res.status(200).send(data.school['curriculums'])
+    }
+  })
+})
+
+router.post('/post_curriculum_list',(req,res)=>{
+  User.findOne({_id:req.body._id},(err,data)=>{
+    if(err){
+      console.log(err)
+      res.status(401).send("DB error, please contact  admin")
+    } else {
+      if(data.school['curriculums'].includes(req.body.curriculum)){
+        res.status(404).send("This school already has this curriculum")
+      } else {
+        User.update({_id:req.body._id},{ $push:{ "school.curriculums": req.body.curriculum}  },function(err,data){
+          if(err){
+            console.log(err)
+          } else {
+            res.status(200).send('Curriculum added')
+          }
+        })
+      }
+    }
+  })
+})
+
 router.post('/register',(req,res)=>{
 let userData = req.body;
 User.find({
-  email: userData.email
+  email: userData.username
 },function(err,result){
  if(err){
     console.log(err)
  } else if(under._.isEmpty(result)){
-   console.log(result)
    User.find({
      username: userData.username
    },function(err,result2){
@@ -68,9 +99,8 @@ User.find({
 })
 
 
-router.post('/login',(req,res)=>{
+router.post('/register_school_login',(req,res)=>{
   let userData = req.body;
-  console.log(userData)
   User.findOne({
     username: userData.username
   },function(error,user){
@@ -85,24 +115,95 @@ router.post('/login',(req,res)=>{
             res.status(401).send('Invalid password')
           } else {
               if((user.roles.indexOf(userData.role_request) > -1)){
-                let payload = {
-                  subject: user._id
+                if(user.roles[0]===3 && user.school.registration_valid===true){
+                  res.status(401).send('You are not authorized to login here')
+                } else {
+                  let payload = {
+                    subject: user._id
+                  }
+                  let token = jwt.sign(payload,"secretKey")
+                  let role = userData.role_request
+                  res.status(200).send({status:false,_id:user._id})
                 }
-                let token = jwt.sign(payload,"secretKey")
-                let role = userData.role_request
-                res.status(200).send({token,user,role})
               } else {
                 res.status(401).send('You are not authorized to login with the selected role')
               }
 
 
           }
-});
+               });
 
       }
     }
 
   })
+
+})
+
+
+
+router.post('/login',(req,res)=>{
+  let userData = req.body;
+  User.findOne({
+    username: userData.username
+  },function(error,user){
+    if(error){
+      console.log(error)
+    } else {
+      if(!user){
+        res.status(401).send('username not found')
+      } else {
+        bcrypt.compare(userData.password, user.password).then(function(resp) {
+          if(resp === false){
+            res.status(401).send('Invalid password')
+          } else {
+              if((user.roles.indexOf(userData.role_request) > -1)){
+                if(user.roles[0]===3 && user.school.registration_valid===false){
+                  res.status(401).send('You are not authorized to login as a school')
+                } else {
+                  let payload = {
+                    subject: user._id
+                  }
+                  let token = jwt.sign(payload,"secretKey")
+                  let role = userData.role_request
+                  res.status(200).send({token,user,role})
+                }
+              } else {
+                res.status(401).send('You are not authorized to login with the selected role')
+              }
+
+
+          }
+               });
+
+      }
+    }
+
+  })
+
+})
+
+router.post('/save_school_data',(req,res)=>{
+ let schoolData = req.body;
+ User.update({_id:schoolData.school_id},{
+   $set:{
+     school: {
+       school_name:schoolData.school_name,
+       principal_first_name:schoolData.principal_first_name,
+       principal_last_name: schoolData.principal_last_name,
+       description:schoolData.description,
+       registration_valid: true
+     }
+   }
+
+ },function(err,result){
+   if(err){
+     res.status(401).send(err)
+   } else {
+     res.status(200).send(result)
+   }
+
+ })
 
 })
 
