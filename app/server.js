8ae0d23fea28c72543684be83873f19f8024f10a
DevRajeev <rjvranjan80@gmail.com>
2022-02-23 20:41:03 +0530

    websocket connection

--- app/server.js
+++ app/server.js
@@ -9,6 +9,7 @@ var morgan = require('morgan');
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 const groupModel = require("./models/chat/group.model");
+const WebSocket = require('ws');
 
 //cors policy
 app.use(function (req, res, next) {
@@ -177,3 +178,35 @@ io.on('connection', (socket) => {
     });
 
   });
+
+  //******************** WebSocket ***************************
+  const wss = new WebSocket.Server({ port: 8081 }, () => {
+    console.log("Signalling server is now listening on port 8081");
+});
+
+wss.broadcast = (ws, data) => {
+  wss.clients.forEach((client) => {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+          client.send(data);
+      }
+  });
+};
+
+wss.on('connection', ws => {
+  console.log(`Client connected. Total connected clients: ${wss.clients.size}`);
+
+  ws.on('message', message => {
+      // msg = JSON.parse(message);
+      console.log(message + "\n\n");
+      wss.broadcast(ws, message);
+  });
+  ws.on('close', ws=> {
+      console.log(`Client disconnected. Total connected clients: ${wss.clients.size}`);
+  })
+
+  ws.on('error', error => {
+      console.log(`Client error. Total connected clients: ${wss.clients.size}`);
+  });
+});
+
+//*********************************** WebSocket Ended***************** */
