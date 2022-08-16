0e5a96928054c22aabf559fb67b099e95b359e49
Rajiv Lochan Sharma <rjvranjan80@gmail.com>
2022-08-16 23:16:31 +0530

    CORS try-4

--- app/server.js
+++ app/server.js
@@ -13,12 +13,28 @@ const WebSocket = require('ws');
 
 //cors policy
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-  next();
+  // origin: ['https://vcampus-api.herokuapp.com/'];
+  // origin: true;
+  // res.header("Access-Control-Allow-Origin", "*");
+  // res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS");
+  // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  // res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  // next();
+  res.header("Access-Control-Allow-Origin", "*")
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested, Content-Type, Accept Authorization"
+    )
+    if (req.method === "OPTIONS") {
+      res.header(
+        "Access-Control-Allow-Methods",
+        "POST, PUT, PATCH, GET, DELETE"
+      )
+      return res.status(200).json({})
+    }
+    next();
 });
+
 app.use(morgan('tiny'));
 app.use(express.static('public'));
 const PORT = process.env.PORT || 3000;
