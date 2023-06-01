8610fd68d501467b93cf3086564c4f7de30b9bd7
Rajiv Lochan Sharma <rjvranjan80@gmail.com>
2023-06-01 17:38:11 +0530

    course changes

--- app/core/config/db.config.js
+++ app/core/config/db.config.js
@@ -1,3 +1,4 @@
 module.exports = {
-    url:"mongodb+srv://georges:vcampus2020@cluster0-fuxau.mongodb.net/vcampus?retryWrites=true&w=majority"
+   // url:"mongodb+srv://dev:Password123@cluster1.rljxzbz.mongodb.net/vcampus-dev?retryWrites=true&w=majority"
+   url:"mongodb+srv://georges:vcampus2020@cluster0-fuxau.mongodb.net/vcampus?retryWrites=true&w=majority"
 }
